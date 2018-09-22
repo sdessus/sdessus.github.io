cea360155ffdde7267a40edad52314e64703d486
Sylvain <sylvain.dessus@gmail.com>
2018-09-22 03:16:04 +0200

    Inital commit

---
+++ content/Depart.md
@@ -0,0 +1,6 @@
+Title: Le grand départ
+Date: 2018-09-15 10:40
+Category: Asie du Sud-Est
+gallery:pics_nala
+
+Commenter le départ.
---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Maïlys et Sylvain'
+SITENAME = 'Des voyages, une aventure'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'fr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+# Specify name of a built-in theme
+THEME = "pelican-themes/medius"
